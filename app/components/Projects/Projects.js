fd6ba3607fa08ad49906926ec9d8768b7103b2bf
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-10-16 22:13:27 -0700

    Adjust project grid alignment

--- app/components/Projects/Projects.js
+++ app/components/Projects/Projects.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './Projects.module.css';
 import Card from '../Card/Card';
 import { projectOne, projectTwo } from '@/data';
+import { fontScript } from '@/app/fonts';
 
 const Projects = () => {
   // console.log(projectOne);
   return (
     <div id='projects' className={styles.wrapper}>
       <div className={styles.maxWidthWrapper}>
-        <h2>My Projects</h2>
+        <h2>
+          <span className={fontScript.className}>My</span> Projects
+        </h2>
         <div className={styles.projectsContainer}>
           <Card project={projectOne} />
           <Card project={projectTwo} />
--- data.js
+++ data.js
@@ -9,7 +9,7 @@ export const projectOne = {
 
 export const projectTwo = {
   title: 'Mini Component Library',
-  imgSrc: '/project2.png',
+  imgSrc: '/yum.png',
   imgAlt: 'screenshot of...',
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered  be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our wh  be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our wh  be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our wh',
